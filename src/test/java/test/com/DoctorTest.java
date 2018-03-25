717201393fda5cace5a5d4f074235e90983f1c34
subarnabera <subarna.sasmal@yahoo.com>
2018-03-25 16:43:55 -0500

    1st time

---
+++ src/test/java/test/com/DoctorTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.qa.myjunitgit.Doctor;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Saurav
+ */
+public class DoctorTest {
+    
+    Doctor doc;
+    
+    
+    public DoctorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        doc=new Doctor();
+        
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void getName() {
+    doc.setName("tom");// setting name
+    assertEquals("tom",doc.getName());//validating name
+    }
+}
